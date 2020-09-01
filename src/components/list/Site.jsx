a66f1508de60401ab3b6068f07898e85594755d0
Raphael <raphael.biedermann@tobit.com>
2020-09-01 09:26:53 +0200

    :sparkles: add links to icons

--- src/components/list/Site.jsx
+++ src/components/list/Site.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './list.scss';
 
 function Site(props) {
+    const handleClick = (id) => {
+        chayns.openUrlInBrowser(`https://chayns.net/${id}`);
+    };
+
     const fullName = props.siteInfo.appstoreName;
     const showName = fullName.length <= 15 ? fullName : `${fullName.substring(0, 12)}...`;
     return (
         <div className="list-item">
-            <div className="icon, background">
+            <div className="icon, background" onClick={() => { handleClick(props.siteInfo.siteId); }}>
                 <div className="icon" style={{ backgroundImage: `url('https://sub60.tobit.com/l/${props.siteInfo.locationId}?size=70')` }}/>
             </div>
             <p>{showName}</p>
